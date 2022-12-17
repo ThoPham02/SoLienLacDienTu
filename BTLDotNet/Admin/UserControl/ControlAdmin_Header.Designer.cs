bd24eb0156e9dbbb0a1c3aa7b53ef6e710f6696f
NguyenHaiDang12 <2021050848@student.humg.edu.vn>
2022-12-17 20:08:26 +0700

    add admin header

--- BTLDotNet/Admin/UserControl/ControlAdmin_Header.Designer.cs
+++ BTLDotNet/Admin/UserControl/ControlAdmin_Header.Designer.cs
@@ -34,22 +34,26 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.BackColor = System.Drawing.Color.SkyBlue;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Berlin Sans FB", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(379, 47);
             this.label1.Margin = new System.Windows.Forms.Padding(24, 12, 24, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(894, 46);
+            this.label1.Size = new System.Drawing.Size(917, 52);
             this.label1.TabIndex = 0;
             this.label1.Text = "Trang Quản Lý Sổ Liên Lạc Điện Tử _ Nhóm01";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // AdminHeaderControl
+            // ControlAdmin_Header
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BTLDotNet.Properties.Resources.background_dep_1;
             this.Controls.Add(this.label1);
-            this.Name = "AdminHeaderControl";
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "ControlAdmin_Header";
             this.Size = new System.Drawing.Size(1905, 150);
             this.ResumeLayout(false);
             this.PerformLayout();
